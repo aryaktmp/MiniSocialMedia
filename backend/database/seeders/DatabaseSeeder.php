1ca28395973ab2e5bdeb6ac18f27104e16ce3ba9
aryaakk <aryatiowasesa1306@gmail.com>
2023-03-11 15:18:06 +0700

    commit final and commit readme

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -5,8 +5,10 @@ namespace Database\Seeders;
 use App\Models\Comments;
 use App\Models\DismissStatus;
 use App\Models\Status;
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -17,9 +19,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
         Comments::factory(10)->create();
-        DismissStatus::factory(10)->create();
         Status::factory(10)->create();
+        User::create([
+            'name' => 'aryakkk',
+            'email' => 'arya@example.com',
+            'email_verified_at' => now(),
+            'password' => bcrypt('admin123'), // password
+            'remember_token' => Str::random(10),
+        ]);
     }
 }
